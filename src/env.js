4191d796f121d9f7072d29d7d492f75d55b729a1
Victor Zidaroiu <victor@zidaroiu.com>
2017-11-04 00:35:37 +0000

    Add eslint, dotenv, axios

---
+++ src/env.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const getEnv = (name) => {
+  const env = process.env[`REACT_APP_${name}`] || '';
+
+  return /\D/g.test(env) ? env : parseInt(env, 10);
+};
+
+const env = [
+  'TODO_LIST_API'
+];
+
+export default env.reduce((envs, envVar) => ({ ...envs, [envVar]: getEnv(envVar) }), {});
